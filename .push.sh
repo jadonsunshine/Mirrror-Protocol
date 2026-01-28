0c02366d6fb9fbe46249302d1149b2a18cfc5c00
Jadonamite <jadonamite@gmail.com>
2026-01-28 16:31:26 +0100

    all

---
+++ .push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Commit message: " msg
+read -p "Push where? (origin | secondary | backup | mirror | all): " target
+
+git add .
+git commit -m "$msg"
+
+if [ $? -eq 0 ]; then
+  git push $target main
+else
+  echo "❌ Commit failed. Fix issues and retry."
+fi
